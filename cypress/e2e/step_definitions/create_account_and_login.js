337f6f7e81acc7d03d070aed86aaf8c966828eee
dbquiroga <day@xcapit.com>
2023-07-25 23:57:15 -0300

    first approach to verifyEmailCode

--- cypress/e2e/step_definitions/create_account_and_login.js
+++ cypress/e2e/step_definitions/create_account_and_login.js
@@ -7,16 +7,27 @@ Given("User is on the website sign up", () => {
 });
 
 When("User simulates the account creation process", () => {
+  const email = "correo_ejemplo@example.com";
+
   loginPage.get.nameInput().type("random");
   loginPage.get.phoneInput().type("000000000");
-  loginPage.get.emailInput().type("hi@hi.com");
+  loginPage.get.emailInput().type(email);
+
   loginPage.get.passInput().type("Qwerty123");
   loginPage.setRandomCountry();
+  loginPage.get.verifyEmailBtn().click();
 
-  // colocar numero
-  //colocar email
-  // colocar pass
-  // click boton verificar
+  loginPage.getAndDecodeXSRFToken();
+
+  loginPage.getEmailCode(email).then((verificationCode) => {
+    if (verificationCode) {
+      loginPage.get.verifyEmailInput().type(verificationCode);
+      loginPage.get.verifyEmailBtn().click();
+      // Continúa con el proceso de creación de cuenta y verifica que el registro se haya completado correctamente.
+    } else {
+      // Manejar el escenario de error, si no se pudo obtener el código de verificación.
+    }
+  });
 });
 
 import { removeLogs } from "../../support/helper/RemoveLogs";
--- cypress/support/pages/login.page.js
+++ cypress/support/pages/login.page.js
@@ -1,35 +1,57 @@
 class Login {
-  get = {
-    nameInput: () => cy.get("#textRegName"),
-    phoneInput: () => cy.get("#phone"),
-    countryphoneInput: () => cy.get("#country_code"),
-    emailInput: () => cy.get("#textSRegEmail"),
-    passInput: () => cy.get("#user_password"),
-  };
+    get = {
+        nameInput: () => cy.get("#textRegName"),
+        phoneInput: () => cy.get("#phone"),
+        countryphoneInput: () => cy.get("#country_code"),
+        emailInput: () => cy.get("#textSRegEmail"),
+        passInput: () => cy.get("#user_password"),
+        verifyEmailBtn: () => cy.get("#validate_email_id"),
+        verifyEmailInput: () => cy.get("#txtEmailValidateOTP"),
+    };
 
-  setRandomCountry() {
-    this.get
-      .countryphoneInput()
-      .click()
-      .find("option")
-      .then((options) => {
-        const randomIndex = Cypress._.random(0, options.length - 1);
-        const randomOption = options[randomIndex];
-        const randomValue = randomOption.value;
-        const randomText = randomOption.text;
-
-        // Intentar hacer clic en la opción aleatoria con { force: true }
-        cy.wrap(randomOption).click({ force: true });
-
-        // Agregar una pausa de 1 segundo antes de hacer la aserción
+    setRandomCountry() {
+        this.get.countryphoneInput().select(Cypress._.random(0, 7));
         cy.wait(1000);
+    }
 
-        // Comprobar si el valor se actualizó en el elemento select
-        cy.get("#country_code").should("have.value", randomValue);
+getEmailCode(email) {
+  return cy
+    .request({
+      method: "POST",
+      url: "https://www.tutorialspoint.com/market/sendEmailOTP.php",
+      body: {
+        email: email,
+      },
+    })
+    .then((response) => {
+      const verificationCode = response.body.verificationCode;
+      cy.log("Código de verificación:", verificationCode);
+      return verificationCode;
+    })
+    .catch((error) => {
+      cy.log("Error al obtener el código de verificación:", error);
+      return null;
+    });
+}
 
-        cy.log(`Selected random country: ${randomValue} (${randomText})`);
-      });
+    
+// Función para obtener y decodificar el valor de la cookie XSRF-TOKEN
+  getAndDecodeXSRFToken() {
+    cy.getCookie('XSRF-TOKEN').then((cookie) => {
+      if (cookie && cookie.value) {
+        const decodedValue = decodeURIComponent(cookie.value);
+        console.log("Decoded XSRF-TOKEN value:", decodedValue);
+        // Aquí puedes realizar alguna manipulación adicional con el valor de la cookie si es necesario
+        // Por ejemplo, puedes almacenar el valor en una variable de la clase o en el contexto de Cypress
+      } else {
+        console.log("Cookie 'XSRF-TOKEN' not found or empty.");
+      }
+    });
   }
+}
+    
+    
+    
 }
 
 export const loginPage = new Login();
